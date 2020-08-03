1eb9621dbe3957ed414a48d955fdb4abdcd20379
songshiyu <ssy3@meitu.com>
2020-08-03 08:55:41 +0800

    实现插入排序的另一种方法

--- src/category/insertsort/Insert.java
+++ src/category/insertsort/Insert.java
@@ -4,6 +4,8 @@ import util.AlgorithmHelper;
 import util.ArrayGenerator;
 import util.BaseAlgotithm;
 
+import java.util.Arrays;
+
 
 /**
  * @author songshiyu
@@ -29,7 +31,7 @@ public class Insert extends BaseAlgotithm {
     /**
      * 优化 每个元素只交换一次位置,通过平移的方式
      */
-    @Override
+   /* @Override
     public <E extends Comparable<E>> void sort(E[] arr) {
         for (int i = 0; i < arr.length; i++) {
             E tmp = arr[i];
@@ -39,14 +41,36 @@ public class Insert extends BaseAlgotithm {
             }
             arr[j] = tmp;
         }
+    }*/
+
+    /**
+     * 从后向前实现插入排序法
+     * */
+    @Override
+    public <E extends Comparable<E>> void sort(E[] arr){
+        for (int i = arr.length -1; i >= 0; i--){
+            E tmp = arr[i];
+            int j;
+            for (j = i; j + 1 < arr.length && tmp.compareTo(arr[j + 1]) > 0;j++){
+                arr[j] = arr[j + 1];
+            }
+            arr[j] = tmp;
+        }
     }
 
+
     public static void main(String[] args) {
-        Integer[] dataSizes = {10000, 100000};
+
+        Integer[] arr = {4,2,1,5,6,3};
+        Insert insert = new Insert();
+        insert.sort(arr);
+        Arrays.stream(arr).forEach(System.out::println);
+
+       /* Integer[] dataSizes = {10000, 100000};
 
         for (Integer dataSize : dataSizes) {
             Integer[] array = ArrayGenerator.generatorRandArray(dataSize, dataSize);
             AlgorithmHelper.sort(Insert.class, "InsertSort", array);
-        }
+        }*/
     }
 }

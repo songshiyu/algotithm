155656e637bbef454ca9e12e3a2c36bf0b005234
songshiyu <ssy3@meitu.com>
2020-08-03 09:34:51 +0800

    创建自己的数组

---
+++ src/category/structure/MyArray.java
@@ -0,0 +1,13 @@
+package category.structure;
+
+/**
+ * @description:
+ * @E-mail: ssy3@meitu.com
+ * @create: 2020/8/3 09:27:46
+ **/
+public class MyArray {
+
+    public static void main(String[] args) {
+
+    }
+}

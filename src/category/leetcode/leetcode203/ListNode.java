4e80e9770361b686c8abb319b558a71f03ae6d24
songshiyu <ssy3@meitu.com>
2020-08-12 10:08:45 +0800

    1.使用链表实现队列 2.解决leetcode第203题

---
+++ src/category/leetcode/leetcode203/ListNode.java
@@ -0,0 +1,14 @@
+package category.leetcode.leetcode203;
+
+/**
+ * @description:
+ * @create: 2020/8/12 09:38:24
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/category/structure/queue/impl/LinkedListQueue.java
@@ -0,0 +1,115 @@
+package category.structure.queue.impl;
+
+import category.structure.linked.MyLinkedListUp;
+import category.structure.queue.Myqueue;
+
+/**
+ * @description: 使用链表实现队列
+ * @create: 2020/8/12 08:45:37
+ **/
+public class LinkedListQueue<E> implements Myqueue<E> {
+
+    private class Node {
+        private E e;
+        private Node next;
+
+        private Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        private Node(E e) {
+            this(e, null);
+        }
+
+        private Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LinkedListQueue() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void enqueue(E e) {
+        if (tail == null) {
+            tail = new Node(e);
+            head = tail;
+        } else {
+            tail.next = new Node(e);
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    @Override
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("can not dequeue from an empty queue");
+        }
+
+        Node retNode = head;
+        head = head.next;
+        retNode.next = null;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return retNode.e;
+    }
+
+    @Override
+    public E getFront() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("queue is empty");
+        }
+        return head.e;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer res = new StringBuffer();
+
+        Node cur = head;
+        while (cur != null) {
+            res.append(cur + "->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedListQueue<Integer> linkedListQueue = new LinkedListQueue<>();
+        for (int i = 0; i < 10; i++) {
+            linkedListQueue.enqueue(i);
+            System.out.println(linkedListQueue);
+            if (i % 3 == 2) {
+                linkedListQueue.dequeue();
+                System.out.println(linkedListQueue);
+            }
+        }
+    }
+}

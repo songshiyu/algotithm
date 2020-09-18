c274ae6459af76023e4c48a5b568b781fc8415c2
songshiyu <ssy3@meitu.com>
2020-09-18 14:30:29 +0800

    深入理解归并排序，添加理解过程

--- src/category/mergesort/MergeSort.java
+++ src/category/mergesort/MergeSort.java
@@ -9,6 +9,26 @@ import java.util.Arrays;
 /**
  * @description: 简陋版的归并排序
  * @create: 2020/8/18 08:22:55
+ *
+ *   int[] arr = {3,1,2,6,5,4,7}
+ *   第一轮：
+ *   int l = 0, int r = 7, int mid = 3
+ *   第二轮：
+ *   int l = 0, int r = 3, int mid = 1 || int l = 4, int r = 7, int mid = 5
+ *   第三轮：
+ *   int l = 0, int r = 1, int mid = 0
+ *   int l = 2, int r = 3, int mid = 2
+ *   int l = 4, int r = 5, int mid = 4
+ *   int l = 6, int r = 7, int mid = 6
+ *   第四轮：
+ *   int l = 0, int r = 0
+ *   int l = 1, int r = 1
+ *   int l = 2, int r = 2
+ *   int l = 3, int r = 3
+ *   int l = 4, int r = 4
+ *   int l = 5, int r = 5
+ *   int l = 6, int r = 6
+ *   int l = 7, int r = 7
  **/
 public class MergeSort extends BaseAlgotithm {
 
@@ -40,6 +60,8 @@ public class MergeSort extends BaseAlgotithm {
 
         E[] temp = Arrays.copyOfRange(arr, l, r + 1);
 
+        //int l = 0, int r = 1, int mid = 0
+        //int i = 0,j = 1,mid = 0,r = 1
         int i = l, j = mid + 1;
         for (int k = l; k <= r; k++) {
             if (i > mid) {
